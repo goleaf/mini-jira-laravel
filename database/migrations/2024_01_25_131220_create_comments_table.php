6da6f6a75fb20f6b05c78cc2170722e75d4f4caf
Andrej Prus <goleaf@gmail.com>
2024-09-19 02:17:22 +0300

    cleanup conflicted files

--- database/migrations/2024_01_25_131220_create_comments_table.php
+++ database/migrations/2024_01_25_131220_create_comments_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Task;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -13,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('task_id')->constrained()->onDelete('cascade');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignIdFor(Task::class)->constrained()->onDelete('cascade');
+            $table->foreignIdFor(User::class)->constrained()->onDelete('cascade');
             $table->text('body');
             $table->unsignedBigInteger('parent_id')->nullable();
             $table->foreign('parent_id')->nullable()->references('id')->on('comments')->onDelete('cascade');
